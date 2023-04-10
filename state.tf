ae8bda56981d84bcdfef0556def23fea51db591a
Thea Urne <theaurn@stud.ntnu.no>
2023-04-10 12:00:38 +0200

    first commit for the artifact branch

--- state.tf
+++ state.tf
@@ -4,12 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
-/**
-    snyk = {
-      source = "snyk/snyk"
-      version = ">= 1.0.0"
-    }
-    */
   }
 
   required_version = ">= 1.3.0"
@@ -19,12 +13,3 @@ provider "aws" {
   profile = "default"
   region  = "eu-north-1"
 }
-
-/**
-
-/**
-provider "snyk" {
-  # Enter your Snyk API token here
-  token = "your-snyk-api-token"
-}
-*/
