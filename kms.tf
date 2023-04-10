265913dcf21ff4c90a5fda8c76efe901466c8631
Thea Urne <theaurn@stud.ntnu.no>
2023-04-10 14:20:55 +0200

    added name and encryption

--- kms.tf
+++ kms.tf
@@ -1,9 +1,15 @@
 resource "aws_kms_key" "pipeline_key" {
-  description = "KMS key for signing pipeline artifacts"
-  customer_master_key_spec = "SYMMETRIC_DEFAULT"
-  is_enabled               = true
+  description               = "KMS key for signing pipeline artifacts"
+  customer_master_key_spec  = "SYMMETRIC_DEFAULT"
+  key_usage                 = "ENCRYPT_DECRYPT"
+  is_enabled                = true
 
   tags = {
     Name = "pipeline_key"
   }
+}
+
+resource "aws_kms_alias" "key_alias" {
+  name          = "alias/pipeline-key"
+  target_key_id = aws_kms_key.pipeline_key.key_id
 }
