c2ddbeae19df78371d42b1aec0ba84945ccf014c
Thea Urne <theaurn@stud.ntnu.no>
2023-04-10 14:00:16 +0200

    creates a kms key

--- kms.tf
+++ kms.tf
@@ -1,4 +1,9 @@
 resource "aws_kms_key" "pipeline_key" {
-  description = "KMS key for encrypting pipeline artifacts"
-  deletion_window_in_days = 30
+  description = "KMS key for signing pipeline artifacts"
+  customer_master_key_spec = "SYMMETRIC_DEFAULT"
+  is_enabled               = true
+
+  tags = {
+    Name = "pipeline_key"
+  }
 }
