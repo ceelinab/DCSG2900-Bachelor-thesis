ce157964a92ed339e4e5dfd545c8c6f5bf092261
Thea Urne <theaurn@stud.ntnu.no>
2023-04-13 17:15:37 +0200

    ec2 tag filter

--- deployment_group.tf
+++ deployment_group.tf
@@ -3,4 +3,10 @@ resource "aws_codedeploy_deployment_group" "deploy_group" {
   deployment_config_name = aws_codedeploy_deployment_config.deployment_config.id
   app_name               = aws_codedeploy_app.codedeploy2.name
   service_role_arn       = aws_iam_role.codedeploy-role.arn
+  
+  ec2_tag_filter {
+    key = "Name"
+    type = "KEY_AND_VALUE"
+    value = "test_instance"
+  }
 }
