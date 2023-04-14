c72dc46bfc736b99139513b5ce37c7c40f8b73d0
Sebastian Hestsveen <sebashes@stud.ntnu.no>
2023-04-14 15:15:31 +0200

    bug fiks in code build

--- code-build.tf
+++ code-build.tf
@@ -1,5 +1,5 @@
 resource "aws_codebuild_project" "build" {
-  name         = var.bucket_name
+  name         = var.build_name
   description  = var.build_desc
   service_role = aws_iam_role.tf-codebuild-role.arn
 
